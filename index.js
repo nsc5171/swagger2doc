13b85bd495d84f22cac6d79036e5ae453e4f11b3
nsc5171 <nsaichandra55@gmail.com>
2019-03-23 01:21:54 +0530

    handlebars tmpl

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const { swagger, info, basePath, paths, definitions, tags, consumes, produces } = require('./swagger.json')
+const asciiStringGenerator = require('./lib/asciidoc/asciiStringGenerator')
+const swagger = require('./swagger.json')
+const constants = require('./lib/util/constants')
+
+
+let stringGen = asciiStringGenerator();
+let context = Object.assign({}, swagger, constants);
+console.log(stringGen(context));
 
