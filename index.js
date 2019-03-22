a7e4770c0152501f44b6f4b932e87cc553eea64c
nsc5171 <nsaichandra55@gmail.com>
2019-03-23 02:22:03 +0530

    asciidoctor beautify

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const asciiStringGenerator = require('./lib/asciidoc/asciiStringGenerator')
+const asciiStringGenerator = require('./lib/asciidoc/asciiStringGenerator')();
 const swagger = require('./swagger.json')
 const constants = require('./lib/util/constants')
+const asciiToDocConverter = require('./lib/asciidoc/asciiToDocConverter')();
+const generateDocFile = require('./lib/util/output/generateDocFile')();
 
-
-let stringGen = asciiStringGenerator();
-let context = Object.assign({}, swagger, constants);
-console.log(stringGen(context));
-
+let context = Object.assign({}, swagger, constants.ASCIIDOCTOR);
+let asciiString = asciiStringGenerator(context);
+// console.log(asciiString);
+generateDocFile(asciiString, "doc.adoc")
+let html = asciiToDocConverter(asciiString);
+// console.log(html);
+generateDocFile(html, "document.html")
