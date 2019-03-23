50a69f1d5ad92edb3ec479a972f411feee1b0609
nsc5171 <nsaichandra55@gmail.com>
2019-03-23 19:41:26 +0530

    commented unnecessary log statement

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const generateDocFile = require('./lib/util/output/generateDocFile')();
 
 let context = Object.assign({}, swagger, constants.ASCIIDOCTOR);
 let asciiString = asciiStringGenerator(context);
-console.log(asciiString);
+// console.log(asciiString);
 generateDocFile(asciiString, "doc.adoc")
 let html = asciiToDocConverter(asciiString);
 // console.log(html);
